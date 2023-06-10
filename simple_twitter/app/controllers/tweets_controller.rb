1faf931a0dbd9ac5c2c79600cade34409708b749
bfigueroa99 <bfigueroa@miuandes.cl>
2023-06-10 16:19:34 -0400

    avance tweets

--- simple_twitter/app/controllers/tweets_controller.rb
+++ simple_twitter/app/controllers/tweets_controller.rb
@@ -11,7 +11,23 @@ class TweetsController < ApplicationController
         @tweet = Tweet.new
     end
 
+    def create
+        tweet = Tweet.new tweet_params
+        tweet.user = current_user
+        if tweet.save
+            redirect_to tweet, notice: "Tweet was successfully created."
+        else
+            render :new
+        end
+    end
+
     def destroy
 
+    end
+
+    private
+
+    def tweet_params
+        params.require(:tweet).permit(:body)
     end
 end
